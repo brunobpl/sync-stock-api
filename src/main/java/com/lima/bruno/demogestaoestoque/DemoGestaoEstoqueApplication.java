466ef5d8146d579b012b4973e07433e837fbe23e
brunobpl <brunopaeselima@gmail.com>
2024-11-22 21:08:11 -0300

    Commit inicial do projeto com estrutura básica e dependências.

---
+++ src/main/java/com/lima/bruno/demogestaoestoque/DemoGestaoEstoqueApplication.java
@@ -0,0 +1,13 @@
+package com.lima.bruno.demogestaoestoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGestaoEstoqueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoGestaoEstoqueApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lima/bruno/demogestaoestoque/DemoGestaoEstoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lima.bruno.demogestaoestoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGestaoEstoqueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
